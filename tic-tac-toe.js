5aa3ee503700e98dbb0f94fe3b395a0f0d29e1af
Oren Argaman <oren1219@gmail.com>
2022-05-10 20:09:50 +0300

    start of check won logic + added alternate players function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,4 +2,27 @@
 
 // PLAYER REGISTER
 
-// ALTERNATE PLAYERSs
+// ALTERNATE PLAYERS
+function alternatePlayers(startPlayer, secondPlayer, moveCount) {
+  let currentPlayer;
+  currentPlayer = moveCount / 2 == 0 ? startPlayer : secondPlayer;
+  return currentPlayer;
+}
+
+function checkWin() {
+  // Check rows
+  board.forEace((row) => {
+    let set1 = new Set(row);
+    if ([...set1].length == 1) {
+      return [...set1][0];
+    }
+  });
+  // Make an array of the columns
+  // Make an array of the diagonals
+}
+
+function match() {}
+
+function game() {
+  alternatePlayers();
+}
