242efc90e62a2f2135197c01a88bc65f90e88643
bmzzzzzzzzzzz <400bmz@gmail.com>
2022-05-12 22:07:04 +0300

    add complited borad add boradArr and push id elemnettoarr

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,79 +1,90 @@
 // BOARD
-let rows = 3;
-let cols = 3;
-const card = ['', '', '']
+let boardSize = 3;
+let boradArr = []
 const board = document.getElementById("board");
 
-function flipCrad() {
-    console.log(card);
+function newArray(size) { //genert new array and return 
+    const newArr = []
+    for (a = 0; a < size; a++) {
+        newArr[a] = ""
+    }
+    return newArr;
 }
-
+function boradArrayConstractor(size) {//push new array in boradArr
+    for (i = 0; i < size; i++) {
+        boradArr[i] = newArray(size);
+    }
+}
+function click() {
+    boradArr[this.id[0]][this.id[1]] = "x"
+    console.log(boradArr);
+}
 function craeteCrad(idx) {
-    const row = document.createElement("div")
-    for (let i = 3; i > 0; i--) {
-        const col = document.createElement("div")
+    for (i = 0; i < idx; i++) {
+        const row = document.createElement("span")
 
-        console.log(i);
-        row.className = 'row'
-        for (let f = 3; f > 0; f--) {
-            col.className = `col  ${f}`
+        row.className = `col${i}`
+        for (f = 0; f < idx; f++) {
+            const col = document.createElement("div")
+            col.className = `row${f}`
             col.innerText = "X"
+            col.id = `${f}${i}`
+            col.addEventListener('click', click)
             row.appendChild(col)
 
         }
         board.appendChild(row);
     }
 
-    // container.addEventListener('click', flipCrad)
 
 }
 
-for (i of card) {
-    craeteCrad(i)
-}
+
+craeteCrad(boardSize)
+boradArrayConstractor(boardSize);
 
 // PLAYER REGISTER
-let moveCounter=0;
+let moveCounter = 0;
 
 // ALTERNATE PLAYERS
 function alternatePlayers(startPlayer, secondPlayer, moveCount) {
-  let currentPlayer;
-  currentPlayer = moveCount / 2 == 0 ? startPlayer : secondPlayer;
-  return currentPlayer;
+    let currentPlayer;
+    currentPlayer = moveCount / 2 == 0 ? startPlayer : secondPlayer;
+    return currentPlayer;
 }
 // This function returns the symbol of the winner
 function checkWin(board) {
-  // n is the matrix dimension
-  // This function checks for rows with equal elemnts
-  function equalElements(row) {
-    let set1 = new Set(row);
-    if ([...set1].length == 1 && [...set1][0] != "") {
-      return [...set1][0];
+    // n is the matrix dimension
+    // This function checks for rows with equal elemnts
+    function equalElements(row) {
+        let set1 = new Set(row);
+        if ([...set1].length == 1 && [...set1][0] != "") {
+            return [...set1][0];
+        }
     }
-  }
 
-  function checkEqual(board) {
-    board.forEace((row) => {
-      equalElements(row);
-    });
-  }
+    function checkEqual(board) {
+        board.forEace((row) => {
+            equalElements(row);
+        });
+    }
 
-  // Check rows
-  checkEqual(board);
-  // Transpose the board to iterate over the columns
-  let boardTrans = board[0].map((col, ind) => board.map((row) => row[ind]));
-  checkEqual(boardTrans);
+    // Check rows
+    checkEqual(board);
+    // Transpose the board to iterate over the columns
+    let boardTrans = board[0].map((col, ind) => board.map((row) => row[ind]));
+    checkEqual(boardTrans);
 
-  // Make an array of the diagonals
-  let mainDiag = board.map((col, ind) => col[ind++]);
-  equalElements(mainDiag);
-  let n = board[0].length;
-  let secDiag = board.map((col, ind) => col[n - ind++]);
-  equalElements(secDiag);
+    // Make an array of the diagonals
+    let mainDiag = board.map((col, ind) => col[ind++]);
+    equalElements(mainDiag);
+    let n = board[0].length;
+    let secDiag = board.map((col, ind) => col[n - ind++]);
+    equalElements(secDiag);
 }
 
-function match() {}
+function match() { }
 
 function game() {
-  alternatePlayers();
+    alternatePlayers();
 }
