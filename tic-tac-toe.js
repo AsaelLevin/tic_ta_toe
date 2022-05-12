ebc2a532d90084df96b3a5801a669e2ef25caa66
bmzzzzzzzzzzz <400bmz@gmail.com>
2022-05-12 22:22:27 +0300

    add test

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,4 @@
 // BOARD
-<<<<<<< HEAD
 let boardSize = 3;
 let boradArr = []
 const board = document.getElementById("board");
@@ -19,18 +18,8 @@ function boradArrayConstractor(size) {//push new array in boradArr
 function click() {
     boradArr[this.id[0]][this.id[1]] = "x"
     console.log(boradArr);
-=======
-let rows = 3;
-let cols = 3;
-const card = ["", "", ""];
-const board = document.getElementById("board");
-
-function flipCrad() {
-  console.log(card);
->>>>>>> c0245ee83ce404e9ad2c54d0f1eedfe789b4a404
 }
 function craeteCrad(idx) {
-<<<<<<< HEAD
     for (i = 0; i < idx; i++) {
         const row = document.createElement("span")
 
@@ -45,37 +34,16 @@ function craeteCrad(idx) {
 
         }
         board.appendChild(row);
-=======
-  const row = document.createElement("div");
-  for (let i = 3; i > 0; i--) {
-    const col = document.createElement("div");
-
-    console.log(i);
-    row.className = "row";
-    for (let f = 3; f > 0; f--) {
-      col.className = `col  ${f}`;
-      col.innerText = "X";
-      row.appendChild(col);
->>>>>>> c0245ee83ce404e9ad2c54d0f1eedfe789b4a404
     }
     board.appendChild(row);
-  }
-
-<<<<<<< HEAD
-
 }
+
+
+
 
 
 craeteCrad(boardSize)
 boradArrayConstractor(boardSize);
-=======
-  // container.addEventListener('click', flipCrad)
-}
-
-for (i of card) {
-  craeteCrad(i);
-}
->>>>>>> c0245ee83ce404e9ad2c54d0f1eedfe789b4a404
 
 // PLAYER REGISTER
 let moveCounter = 0;
@@ -97,19 +65,11 @@ function checkWin(board) {
         }
     }
 
-<<<<<<< HEAD
     function checkEqual(board) {
         board.forEace((row) => {
             equalElements(row);
         });
     }
-=======
-  function checkEqual(board) {
-    board.map((row) => {
-      equalElements(row);
-    });
-  }
->>>>>>> c0245ee83ce404e9ad2c54d0f1eedfe789b4a404
 
     // Check rows
     checkEqual(board);
@@ -125,20 +85,16 @@ function checkWin(board) {
     equalElements(secDiag);
 }
 
-<<<<<<< HEAD
-function match() { }
-=======
 function match() {
-  
+
 }
 
-let restart=document.querySelector(".playAgain");
+let restart = document.querySelector(".playAgain");
 
 
-function playAgain(){
-  
+function playAgain() {
+
 }
->>>>>>> c0245ee83ce404e9ad2c54d0f1eedfe789b4a404
 
 function game() {
     alternatePlayers();
