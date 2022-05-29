ca6756cd35ac47cd4f5a5dbe43bcd88b1ba4e3a5
bmzzzzzzzzzzz <400bmz@gmail.com>
2022-05-29 15:29:56 +0300

    stammm

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,22 +9,22 @@ function currentPlayerfanc(name) {
 }
 
 function newArray(size) {
-  //genert new array and return
-  const newArr = [];
-  for (a = 0; a < size; a++) {
-    newArr[a] = "";
-  }
-  return newArr;
+    //genert new array and return
+    const newArr = [];
+    for (a = 0; a < size; a++) {
+        newArr[a] = "";
+    }
+    return newArr;
 }
 function boradArrayConstractor(size) {
-    //push new array in boradArr
+    //push new array in boardArr
     for (i = 0; i < size; i++) {
         boardArr[i] = newArray(size);
     }
 }
-function click() {
+function clickbtn1() {
 
-    this.removeEventListener('click', click)
+    this.removeEventListener('click', clickbtn1)
     let currentPlayer = alternatePlayers(moveCounter);
     currentPlayerfanc(alternatePlayers(moveCounter))//input player name to screen
     if (moveCounter % 2 == 0) {
@@ -40,6 +40,7 @@ function click() {
 
     let checkInd = Number(gameData.rangeInput) * 2 - 1;
     if (moveCounter >= checkInd) {
+
         return checkWin(boardArr);
     }
     return currentPlayer;
@@ -55,15 +56,15 @@ function craeteCrad(idx) {
             col.className = `col-3 divi`
             col.innerText = ""
             col.id = `${i}${f}`
-            col.addEventListener('click', click)
+            col.addEventListener('click', clickbtn1)
             row.appendChild(col)
 
         }
         board.appendChild(row);
     }
-    board.appendChild(row);
-  }
-  // board.appendChild(row);
+
+}
+
 
 
 function test() { }
@@ -75,10 +76,15 @@ boradArrayConstractor(boardSize);
 let moveCounter = 0;
 
 // ALTERNATE PLAYERS
-function alternatePlayers(startPlayer, secondPlayer, moveCount) {
-  let currentPlayer;
-  currentPlayer = moveCount / 2 == 0 ? startPlayer : secondPlayer;
-  return currentPlayer;
+function alternatePlayers(moveCount) {
+    let currentPlayer;
+    let isEven = moveCount % 2;
+    currentPlayer = isEven ? gameData.name1 : gameData.name2;
+    return currentPlayer;
+}
+
+function win(winner) {
+    alert(winner);
 }
 // This function returns the symbol of the winner
 function checkWin(board) {
@@ -90,60 +96,51 @@ function checkWin(board) {
         if ([...set1].length == 1 && [...set1][0] != "") {
             return [...set1][0];
         }
-
-        function checkEqual(board1) {
-            board1.map((row) => {
-                isWin = equalElements(row);
-                if (isWin) {
-                    win(isWin);
-                }
-            });
-        }
-
-        // Check rows
-        checkEqual(board1);
-        // Transpose the board to iterate over the columns
-        let boardTrans = board1[0].map((col, ind) => board1.map((row) => row[ind]));
-        checkEqual(boardTrans);
-
-        // Make an array of the diagonals
-        let mainDiag = board1.map((col, ind) => col[ind++]);
-        isWin = equalElements(mainDiag);
-        if (isWin) {
-            win(isWin);
-        }
-        let n = board[0].length - 1;
-        let secDiag = board1.map((col, ind) => col[n - ind++]);
-        isWin = equalElements(secDiag);
-        if (isWin) {
-            win(isWin);
-        }
     }
-  }
-
-  function checkEqual(board) {
-    board.map((row) => {
-      equalElements(row);
-    });
-  }
-
-    function playAgain() {
-        let restart = document.querySelector(".playAgain");
+    function checkEqual(board) {
+        board.map((row) => {
+            isWin = equalElements(row);
+            if (isWin) {
+                win(isWin);
+            }
+        });
     }
 
-    function getUrlData() {
-        let gameData = [],
-            hash;
-        let hashes = window.location.href
-            .slice(window.location.href.indexOf("?") + 1)
-            .split("&");
-        for (let i = 0; i < hashes.length; i++) {
-            hash = hashes[i].split("=");
-            gameData[hash[0]] = hash[1];
-        }
-        return gameData;
+    // Check rows
+    checkEqual(board);
+    // Transpose the board to iterate over the columns
+    let boardTrans = board[0].map((col, ind) => board.map((row) => row[ind]));
+    checkEqual(boardTrans);
+
+    // Make an array of the diagonals
+    let mainDiag = board.map((col, ind) => col[ind++]);
+    isWin = equalElements(mainDiag);
+    if (isWin) {
+        win(isWin);
     }
+    let n = board[0].length - 1;
+    let secDiag = board.map((col, ind) => col[n - ind++]);
+    isWin = equalElements(secDiag);
+    if (isWin) {
+        win(isWin);
+    }
+}
 
+function match() { }
 
+function playAgain() {
+    let restart = document.querySelector(".playAgain");
+}
 
-
+function getUrlData() {
+    let gameData = [],
+        hash;
+    let hashes = window.location.href
+        .slice(window.location.href.indexOf("?") + 1)
+        .split("&");
+    for (let i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split("=");
+        gameData[hash[0]] = hash[1];
+    }
+    return gameData;
+}
